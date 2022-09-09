6c9c9841d1912a2ddf601ea58f08c60b94d8d446
Kepsondiaz <kherabadiaby328@gmail.com>
2022-09-09 15:57:25 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
-
 /**
- * main - prints all possible combinations of two two-digit numbers.
+ * main - program that prints all possible different combinations
+ * of thress digits.
  *
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
+	int n;
 	int j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (n = 48; n < 58; n++)
 		{
-			if (j > i)
+			if (n > i)
 			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
-				putchar(' ');
-				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
-
-				if (i * 100 + j != 9899)
+				for (j = 48; j < 58; j++)
 				{
+					if (j > n)
+					{
+					putchar(i);
+					putchar(n);
+					putchar(j);
+					if (i == 55 && n == 56 && j == 57)
+					{
+						break;
+					}
+
 					putchar(',');
 					putchar(' ');
+					}
 				}
 			}
 		}
 	}
-	putchar('\n');
 
+	putchar('\n');
 	return (0);
 }
